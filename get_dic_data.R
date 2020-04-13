17342fe0dd64abd0ffac193e501d9cd7c022463c
Brett Longworth <blongworth@gmail.com>
2020-04-13 09:18:11 -0400

    Change to custom function to get ws_method

--- get_dic_data.R
+++ get_dic_data.R
@@ -8,8 +8,32 @@ library(blanks)
 con <- conNOSAMS()
 standards <- getStdTable()
 
-wstd <- getNorm(from = as.Date("2019-01-01"), 
-                recs = c(1082, 17185, 83028, 159579)) %>%
+getWS <- function(recnum) {
+  db <- conNOSAMS()
+  query <- glue::glue_sql("SELECT wheel, wheel_pos, sample_name,
+                             tp_date_pressed, target.tp_num, target.rec_num,
+                             target.osg_num, gf_devel, gf_test, ws_r_d,
+                             ws_method_num, ws_line_num, ws_strip_date,
+                             ws_comments, ws_comment_code,
+                             norm_ratio, int_err, ext_err, fm_corr, 
+                             sig_fm_corr, dc13, total_umols_co2, sig_tot_umols
+                          FROM snics_results
+                          JOIN target ON snics_results.tp_num = target.tp_num
+                          JOIN graphite ON target.osg_num = graphite.osg_num
+                          JOIN dc13 ON snics_results.tp_num = dc13.tp_num
+                          LEFT JOIN water_strip ON graphite.ws_num = water_strip.ws_num
+                          WHERE target.rec_num IN ({recnums*})",
+                          recnums = recnum,
+                          .con = db
+  )
+  
+  recs <- odbc::dbSendQuery(db, query)
+  data <- odbc::dbFetch(recs)
+  odbc::dbClearResult(recs)
+  data
+}
+
+wstd <- getWS(c(1082, 17185, 83028, 159579)) %>%
   mutate(process = purrr::map_chr(tp_num, amstools::getProcess, con),
          system = substr(wheel, 1, 5)) %>%
   filter(process == "WS") %>%
