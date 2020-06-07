c6f72cf35c7d116a6c2f8529f1322c0f4206ce8c
Brett Longworth <blongworth@gmail.com>
2020-06-07 10:37:16 -0400

    Drop watson wheel.

--- get_snicser_data.R
+++ get_snicser_data.R
@@ -6,7 +6,7 @@ library(here)
 library(odbc)
 library(glue)
 
-wheels <- c("CFAMS020620", "CFAMS021120", "CFAMS021320", 
+wheels <- c("CFAMS020620", "CFAMS021120", 
             "CFAMS021820", "CFAMS022620", "CFAMS031020") 
 
 ## Get data
