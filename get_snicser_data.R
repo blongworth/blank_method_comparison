334d186d14deed6f6a7ec3cb4df32ad51be26790
Brett Longworth <blongworth@gmail.com>
2020-06-07 17:37:51 -0400

    Get all data necessary for MBC.

--- get_snicser_data.R
+++ get_snicser_data.R
@@ -20,28 +20,48 @@ whdata <- rbind(whdata, whdatat)
 write_csv(whdata, here("data/results.csv"))
 
 #### Get subset of data
-con <- conNOSAMS()
 
-# Function to pull subset of data
-getBCWheel <- function(table, wheels) {
-  query <- glue_sql("SELECT wheel, wheel_pos, target.tp_num, rec_num,
-                       ss, sample_name, norm_method, sample_type, total_umols_co2,
-                       norm_ratio, fm_corr, sig_fm_corr, lg_blk_fm, sig_lg_blk_fm,
-                       fm_mb_corr, sig_fm_mb_corr 
-                     FROM {`table`}
-                     JOIN target ON {`table`}.tp_num = target.tp_num
-                     JOIN dc13_test ON {`table`}.tp_num = dc13_test.tp_num
-                     WHERE wheel in ({wheels*})",
-                    table = table,
-                    wheels = wheels,
-                    .con = con)
+# Function to pull subset of data needed for blank corr
+getBCWheel <- function(wheels, test = FALSE) {
+  if (test) {
+    query <- glue_sql("SELECT wheel, wheel_pos, target.tp_num, rec_num,
+                         ss, sample_name, norm_method, sample_type, total_umols_co2,
+                         sig_tot_umols, mass_cont, mass_cont_err, fm_cont, fm_cont_err,
+                         norm_ratio, fm_corr, sig_fm_corr, lg_blk_fm, sig_lg_blk_fm,
+                         fm_mb_corr, sig_fm_mb_corr 
+                       FROM snics_results_test
+                       JOIN target ON snics_results_test.tp_num = target.tp_num
+                       JOIN dc13_test ON snics_results_test.tp_num = dc13_test.tp_num
+                       WHERE wheel in ({wheels*})",
+                      wheels = wheels,
+                      .con = con)
+  } else {
+    query <- glue_sql("SELECT wheel, wheel_pos, target.tp_num, rec_num,
+                         ss, sample_name, norm_method, sample_type, total_umols_co2,
+                         sig_tot_umols, mass_cont, mass_cont_err, fm_cont, fm_cont_err,
+                         norm_ratio, fm_corr, sig_fm_corr, lg_blk_fm, sig_lg_blk_fm,
+                         fm_mb_corr, sig_fm_mb_corr 
+                       FROM snics_results
+                       JOIN target ON snics_results.tp_num = target.tp_num
+                       JOIN dc13 ON snics_results.tp_num = dc13.tp_num
+                       WHERE wheel in ({wheels*})",
+                      wheels = wheels,
+                      .con = con)
+  }
+  
+  con <- conNOSAMS()
   dbGetQuery(con, query) %>%
-    mutate(table = table,
+    mutate(mass = total_umols_co2 * 12.015,
+           sig_mass = sig_tot_umols * 12.015,
            process = map_chr(tp_num, getProcess, con))
 }
 
 # Get the data
-data <- map_dfr(c("snics_results", "snics_results_test"), getBCWheel, wheels)
+data <- getBCWheel(wheels, test = FALSE) %>%
+  mutate(table = "snics_results")
+datat <- getBCWheel(wheels, test = TRUE) %>%
+  mutate(table = "snics_results_test")
+data <- rbind(data, datat)
 
 # Write to a file
 write_csv(data, here("data/SNICSer_blank_compare.csv"))
