52f02e8aae8ed2a811a29a549c2fa86696bbb3ed
Brett Longworth <blongworth@gmail.com>
2020-06-07 21:48:12 -0400

    Calc R MBC, import MR Excel MBC, add consensus values.

--- get_snicser_data.R
+++ get_snicser_data.R
@@ -6,10 +6,11 @@ library(here)
 library(odbc)
 library(glue)
 
-wheels <- c("CFAMS020620", "CFAMS021120", 
-            "CFAMS021820", "CFAMS022620", "CFAMS031020") 
+# skip CFAMS020620, something wierd with fm_corr, even though all params seem right
 
-## Get data
+wheels <- c("CFAMS021120", "CFAMS021820", "CFAMS022620", "CFAMS031020") 
+
+## Get contents of snics_results and snics_results_test for all test wheels
 
 #### Pull and save all data for wheels
 whdata <- getWheel(wheels) %>%
@@ -17,12 +18,13 @@ whdata <- getWheel(wheels) %>%
 whdatat <- getWheel(wheels, test = TRUE) %>%
   mutate(table = "snics_results_test")
 whdata <- rbind(whdata, whdatat)
-write_csv(whdata, here("data/results.csv"))
+write_csv(whdata, here("data/MBC_compare_snics_results.csv"))
 
-#### Get subset of data
+#### Get subset of data for analysis
 
 # Function to pull subset of data needed for blank corr
 getBCWheel <- function(wheels, test = FALSE) {
+  con <- conNOSAMS()
   if (test) {
     query <- glue_sql("SELECT wheel, wheel_pos, target.tp_num, rec_num,
                          ss, sample_name, norm_method, sample_type, total_umols_co2,
@@ -49,7 +51,6 @@ getBCWheel <- function(wheels, test = FALSE) {
                       .con = con)
   }
   
-  con <- conNOSAMS()
   dbGetQuery(con, query) %>%
     mutate(mass = total_umols_co2 * 12.015,
            sig_mass = sig_tot_umols * 12.015,
@@ -62,6 +63,40 @@ data <- getBCWheel(wheels, test = FALSE) %>%
 datat <- getBCWheel(wheels, test = TRUE) %>%
   mutate(table = "snics_results_test")
 data <- rbind(data, datat)
+
+# add R and MR calculations
+data <- data %>% 
+  mutate(fm_mb_corr_r = pmap_dbl(list(.$fm_corr, .$fm_cont, 
+                                 .$mass, .$mass_cont), 
+                            doMBC),
+         sig_fm_mb_corr_r = pmap_dbl(list(.$fm_corr, .$fm_cont, 
+                               .$mass, .$mass_cont, 
+                               .$sig_fm_corr, .$fm_cont_err, 
+                               .$sig_mass, .$mass_cont_err), 
+                               doMBCerr))
+
+mrdata <- read_excel(here("data/CFAMS03102 MBC Test Results.xlsx"), skip = 3, col_names = FALSE) %>%
+  .[c(3, 25, 26)] %>%
+  rename(tp_num = "...3",
+         fm_mb_corr_mr = "...25",
+         sig_fm_mb_corr_mr= "...26")
+
+data <- data %>% 
+  left_join(mrdata)
+
+# Add consensus data
+
+# get consensus values
+cons <- getStdTable() %>%
+  select(rec_num, fm_consensus)
+
+# merge with data by tp. 
+data <- data %>% 
+  left_join(cons, by = "rec_num") %>%
+  mutate(Fm_reported = ifelse(is.na(fm_mb_corr), fm_corr, fm_mb_corr),
+         sig_Fm_reported = ifelse(is.na(sig_fm_mb_corr), sig_fm_corr, sig_fm_mb_corr),
+         normFm = normFm(Fm_reported, fm_consensus),
+         sigma = sigma(Fm_reported, fm_consensus, sig_Fm_reported))
 
 # Write to a file
 write_csv(data, here("data/SNICSer_blank_compare.csv"))
